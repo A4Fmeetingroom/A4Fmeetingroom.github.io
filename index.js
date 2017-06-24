6717574e34b0695c571fd2e55a49adf42d9428a5
A4Fmeetingroom <A4Fmeetingroom@users.noreply.github.com>
2017-06-25 08:30:51 +0900

    nefryとmilkcocoa

---
+++ index.js
@@ -0,0 +1,15 @@
+var result = prompt( "Milkcocoa‚ÌAPPID‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B" , "" );
+if(result){
+	console.log(" OK ‚ª‰Ÿ‚³‚ê‚½:" + result);
+    var milkcocoa = new MilkCocoa(result+".mlkcca.com");
+    var ds = milkcocoa.dataStore('nefry');
+    ds.on('push', function(pushed) {
+        if(pushed.value.sw=='1'){        
+           document.getElementById("cb").checked = true;
+        }else if(pushed.value.sw=='0'){
+            document.getElementById("cb").checked = false;
+        }
+    });
+}else{
+	console.log(" CANCEL ‚ª‰Ÿ‚³‚ê‚½");
+}
