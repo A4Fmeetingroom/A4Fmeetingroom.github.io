94e2a5d0aa96e2fc4d063cd11e8c1f029c2895b5
A4Fmeetingroom <A4Fmeetingroom@users.noreply.github.com>
2017-06-25 11:01:23 +0900

    test

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var result = prompt( "Milkcocoa‚ÌAPPID‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B" , "" );
 if(result){
 	console.log(" OK ‚ª‰Ÿ‚³‚ê‚½:" + result);
     //var milkcocoa = new MilkCocoa(result+".mlkcca.com");
-    var milkcocoa = new MilkCocoa('hotj38fxucb.mlkcca.com');
+    //var milkcocoa = new MilkCocoa('hotj38fxucb.mlkcca.com');
+    var milkcocoa = MilkCocoa.connectWithApiKey(result+".mlkcca.com", "EBMFKPMCMGLNAHNN", "VicgLgOFJSOIaPaNINETIIOHCOKPPDCDcIlNENHP");
 
 
     var ds = milkcocoa.dataStore('esp8266');
