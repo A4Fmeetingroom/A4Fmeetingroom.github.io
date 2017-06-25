490e8b8787782597d28848497c2a4d3819d9a588
A4Fmeetingroom <A4Fmeetingroom@users.noreply.github.com>
2017-06-25 09:49:30 +0900

    test6

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var result = prompt( "Milkcocoa‚ÌAPPID‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B" , "" );
 if(result){
 	console.log(" OK ‚ª‰Ÿ‚³‚ê‚½:" + result);
-    var milkcocoa = new MilkCocoa(result+".mlkcca.com");
+    //var milkcocoa = new MilkCocoa(result+".mlkcca.com");
+    var milkcocoa = new MilkCocoa('hotj38fxucb.mlkcca.com');
+
+
     var ds = milkcocoa.dataStore('esp8266');
     ds.on('send', function(pushed) {
         if(pushed.value.Room 9=='1'){        
