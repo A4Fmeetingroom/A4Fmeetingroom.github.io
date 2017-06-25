f0681a31dbcdc18f839499b43cca0a2edec28973
A4Fmeetingroom <A4Fmeetingroom@users.noreply.github.com>
2017-06-26 00:24:05 +0900

    console

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var result = prompt( "MilkcocoaÇÃAPPIDÇì¸óÕÇµÇƒÇ≠ÇæÇ≥Ç¢ÅB" , "" );
 if(result){
 	console.log(" OK Ç™âüÇ≥ÇÍÇΩ:" + result);
-    //var milkcocoa = new MilkCocoa(result+".mlkcca.com");
+    var milkcocoa = new MilkCocoa(result+".mlkcca.com");
     //var milkcocoa = new MilkCocoa('hotj38fxucb.mlkcca.com');
     //var milkcocoa = MilkCocoa.connectWithApiKey(result+".mlkcca.com", "EBMFKPMCMGLNAHNN", "VicgLgOFJSOIaPaNINETIIOHCOKPPDCDcIlNENHP");
-      var milkcocoa = MilkCocoa.connectWithApiKey("hotj38fxucb.mlkcca.com", "EBMFKPMCMGLNAHNN", "VicgLgOFJSOIaPaNINETIIOHCOKPPDCDcIlNENHP");
+    //  var milkcocoa = MilkCocoa.connectWithApiKey("hotj38fxucb.mlkcca.com", "EBMFKPMCMGLNAHNN", "VicgLgOFJSOIaPaNINETIIOHCOKPPDCDcIlNENHP");
 
     var ds = milkcocoa.dataStore('esp8266');
     ds.on('send', function(sent) {
