6ebf9c4fa0d6a1cd2f84d1616ca7effe6fa1b6ff
A4Fmeetingroom <A4Fmeetingroom@users.noreply.github.com>
2017-06-26 00:46:53 +0900

    span

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ if(result){
     var ds = milkcocoa.dataStore('esp8266');
     ds.on('send', function(sent) {
     	console.log(sent.value);
+    	
+    	var last_msg = msgs[msgs.length - 1];
+    // 例えばメッセージが{"temperature": 20.0}みたいな形式とする
+    var Room 9 = last_msg.value.Room 9;
+    // spanに温度を表示
+    $('#Room 9').text(Room 9);
+    	
+    	
+    	
     	/*
         if(pushed.value.Room 9=='1'){        
            document.getElementById("Room 9").checked = true;
