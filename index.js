3e7ce0e4f4ea820424289899db753af9f5d3eb5c
A4Fmeetingroom <ichiro.kadota@gmail.com>
2017-06-28 23:13:51 +0900

    ie

--- index.js
+++ index.js
@@ -4,30 +4,20 @@ window.onload = function () {
 	
     //var result = prompt( "MilkcocoaのAPPIDを入力してください。" , "" );
    //if(result){	
-	//console.log(" OK が押された:" + result);
-    //var milkcocoa = new MilkCocoa(result+".mlkcca.com");
+	//console.log(" OK が押された:" + result);   
     //var milkcocoa = MilkCocoa.connectWithApiKey(result+".mlkcca.com", "EBMFKPMCMGLNAHNN", "VicgLgOFJSOIaPaNINETIIOHCOKPPDCDcIlNENHP");
 var milkcocoa = MilkCocoa.connectWithApiKey('hotj38fxucb.mlkcca.com', 'EBMFKPMCMGLNAHNN', 'VicgLgOFJSOIaPaNINETIIOHCOKPPDCDcIlNENHP');
 var ds = milkcocoa.dataStore('esp8266');              // データ取得先のデータストア 
 	
 //画像を配列に格納する
-var img = new Array();
-
-//img[0] = new Image();
-//img[0].src = "images/free.png";
-//img[1] = new Image();
-//img[1].src = "images/busy.png";
-
 img = new Array("images/free.png","images/busy.png");	
 	
 ds.on('send', changeViewFromSentMode);
 	
 function changeViewFromSentMode(sent){
 
-    	console.log(sent.value);
+    	//console.log(sent.value);    	
     	
-    	var image = new Image();
-
     	if(sent.value.Room9=='1'){        
            document.getElementById("Room9").innerHTML = "会議室9：　使用中です　　最終更新日時：　" + sent.value.LastUpdatedTime; 
            //document.getElementById("Room9").innerHTML = "会議室9：　使用中です"; 
@@ -40,12 +30,10 @@ function changeViewFromSentMode(sent){
            //document.getElementById("Room9").innerHTML = "会議室9：　空いています";
 	   //document.getElementById("Room9").src=img[0].src;  //画像を切り替える
 	   document.Room9.src = img[0];
-        }
-           //document.getElementById("LastUpdatedtime").innerHTML = "最終更新日時：　" + sent.value.LastUpdatedTime; 
-	
+        }	
 }
     
-    document.getElementById("Room9").src=img[1].src;  //とりあえず、条件にかかわらず画像を切り替える
+   // document.getElementById("Room9").src=img[1].src;  //とりあえず、条件にかかわらず画像を切り替える
 //}else{
 	//console.log(" CANCEL が押された");
 //}
