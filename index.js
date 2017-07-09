00f2ff65ce7a236357dd9842d0db006e22df2e2d
A4Fmeetingroom <A4Fmeetingroom@users.noreply.github.com>
2017-07-10 00:14:17 +0900

    nikki

--- index.js
+++ index.js
@@ -80,14 +80,12 @@ function changeViewFromSentMode(sent){
         }
     	// Room 9 --------------------------------------------------------------
     	if(sent.value.Room9=='2'){
-    	     document.room9.src = img[1];
+    	     //document.room9.src = img[1];
     	     document.getElementById("joutai9").innerHTML = "使用中";
     	     document.getElementById("joutai9").style.backgroundColor = '#ff4444';
-  
-
         }
         if(sent.value.Room9=='1'){
-             document.room9.src = img[0];
+             //document.room9.src = img[0];
              document.getElementById("joutai9").innerHTML = "空室";
              document.getElementById("joutai9").style.backgroundColor = '#22ff44';
         }
