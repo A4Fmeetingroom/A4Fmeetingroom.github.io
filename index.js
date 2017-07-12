7e49b1d76d694728c74bb4d03c2800dc76dedefc
A4Fmeetingroom <A4Fmeetingroom@users.noreply.github.com>
2017-07-13 00:21:22 +0900

    wakewakame

--- index.js
+++ index.js
@@ -84,16 +84,16 @@ function changeViewFromSentMode(sent){
     	     //document.room9.src = img[1];
     	     document.getElementById("joutai9").innerHTML = "使用中";
     	     document.getElementById("joutai9").style.backgroundColor = '#bb2222';
-    	     document.getElementById("joutai9").style.visibility="visible";
-    	     document.getElementById("moji9").style.visibility="visible";
-             document.getElementById("color9").style.visibility="visible";
+    	     //document.getElementById("joutai9").style.visibility="visible";
+    	     //document.getElementById("moji9").style.visibility="visible";
+             //document.getElementById("color9").style.visibility="visible";
         }
         if(sent.value.Room9=='1'){
              //document.room9.src = img[0];
              document.getElementById("joutai9").innerHTML = "空室";
              document.getElementById("joutai9").style.backgroundColor = '#22bb22';
-             document.getElementById("joutai9").style.visibility="visible";
-             document.getElementById("color9").style.visibility="visible";
+             //document.getElementById("joutai9").style.visibility="visible";
+             //document.getElementById("color9").style.visibility="visible";
         }
         if(sent.value.Room9 >'0'){
         	 var m = new moment(); //現在の時刻が入る
