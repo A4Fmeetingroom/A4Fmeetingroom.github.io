ddbfb7c27019903f52feec530e385aa6bcda6e80
A4Fmeetingroom <A4Fmeetingroom@users.noreply.github.com>
2017-07-12 23:52:47 +0900

    joutai9

--- index.js
+++ index.js
@@ -81,9 +81,9 @@ function changeViewFromSentMode(sent){
     	// Room 9 --------------------------------------------------------------
     	if(sent.value.Room9=='2'){
     	     //document.room9.src = img[1];
-    	     document.getElementById("moji9").innerHTML = "使用中";
+    	     document.getElementById(""joutai9").innerHTML = "使用中";
     	     document.getElementById("color9").style.backgroundColor = '#bb2222';
-    	     //document.getElementById("joutai9").style.visibility="visible";
+    	     document.getElementById("joutai9").style.visibility="visible";
     	     document.getElementById("moji9").style.visibility="visible";
              document.getElementById("color9").style.visibility="visible";
         }
